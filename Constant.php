05800586776dccc971ce43c6808cf94a0b152b2f
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-01-31 08:56:26 +0700

    "this" keyword and constant in PHP OOP

---
+++ Constant.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "data/Series.php";
+define("APPLICATION", "learn PHP OOP");
+const APP_VERSION = "1.0.0";
+
+echo APPLICATION . PHP_EOL;
+echo APP_VERSION . PHP_EOL;
+
+echo Series::WRITER. PHP_EOL;
--- data/Series.php
+++ data/Series.php
@@ -2,11 +2,18 @@
 
 class Series
 {
+    const WRITER = "Keiichi Hasegawa";
     var string $title;
     var ?string $series_writer = null;
     var string $series_producer = "Yosuke Minato";
 
     function seriesTitle(string $title){
-        echo "The series is titled $title" . PHP_EOL;
+        if(is_null($title)){
+            echo "The series is titled $title" . PHP_EOL;
+        }
+        else{
+            echo "The series after $title is {$this->title}" . PHP_EOL;
+        }
+
     }
 }
